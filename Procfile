d5f5e7041ae5cf7a9e21733eed4a1d29ff1ffc97
Mateo850 <mateusg3576@gmail.com>
2025-11-17 17:06:38 -0500

    Agregando Procfile y ajustes en __init__.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn __init__:app --host 0.0.0.0 --port $PORT
--- __init__ .py
+++ __init__ .py
@@ -171,8 +171,6 @@ def obtener_usuario(id_user):
         print("ERROR EN LA EJECUCIÓN DE LA API:", e)
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
-   app.run(host="0.0.0.0",port=8080)
-    
-#if __name__ == '__main__':
- #   app.run(debug=True)
+    app.run(debug=True)
